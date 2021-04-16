a26d8cec4e9d6fea226da18bdb214044e58f33d1
Beimar Alarcon <alarconbeimar@gmail.com>
2021-04-16 10:58:01 -0500

    Create db.php

---
+++ Modelo/db.php
@@ -0,0 +1,6 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'usuario');//Usuario de tu base de datos
+define('DB_PASS', 'contrasena');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'nombre');//Nombre de la base de datos
