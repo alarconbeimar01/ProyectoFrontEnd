ed0103091775a06c91f9d172836d662c22d4892f
Beimar Alarcon <alarconbeimar@gmail.com>
2021-04-16 10:54:17 -0500

    Create menu.php

---
+++ css/menu.php
@@ -0,0 +1,36 @@
+<link href="css/menu.css" rel="stylesheet" type="text/css" media="all"/>
+
+
+<div id="menu">
+<ul>
+  <li class="nivel1"><a href="#" class="nivel1">Facturas</a>
+<!--[if lte IE 6]><a href="http://www.forosdelweb.com" class="nivel1ie">Opción 1<table class="falsa"><tr><td><![endif]-->
+	<ul>
+		<li><a href="file:///C|/Users/alarc.ANDROLDG4/Documents/guarderia/includes/indexf.php">Generar Factura</a></li>
+		
+        <li><a href="file:///C|/Users/alarc.ANDROLDG4/Documents/guarderia/includes/listarFactura.php">Listar Factura</a></li>
+        <li><a href="file:///C|/Users/alarc.ANDROLDG4/Documents/guarderia/includes/validador/modificarEmpresa.php">Editar Empresa</a></li>
+	</ul>
+<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+  </li>
+<ul>
+  <li class="nivel1"><a href="#" class="nivel1">Clientes</a>
+<!--[if lte IE 6]><a href="http://www.forosdelweb.com" class="nivel1ie">Opción 1<table class="falsa"><tr><td><![endif]-->
+	<ul>
+		<li><a href="file:///C|/Users/alarc.ANDROLDG4/Documents/guarderia/includes/insertarAmo.php">Crear Cliente</a></li>
+		<li><a href="file:///C|/Users/alarc.ANDROLDG4/Documents/guarderia/includes/listarAmos.php">Listar Cliente</a></li>
+	</ul>
+<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+  </li>
+  <li class="nivel1"><a href="#" class="nivel1">Mascotas</a>
+<!--[if lte IE 6]><a href="#" class="nivel1ie">Opción 2<table class="falsa"><tr><td><![endif]-->
+	<ul>
+		<li><a href="file:///C|/Users/alarc.ANDROLDG4/Documents/guarderia/includes/insertarMascota.php">Crear Mascota</a></li>
+		<li><a href="file:///C|/Users/alarc.ANDROLDG4/Documents/guarderia/includes/listarCan.php">Listar Mascota</a></li>
+		
+	</ul>
+<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+</li>
+ 
+
+</div>
